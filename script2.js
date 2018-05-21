4bda25bc14bdb496f46e936bfa1ad9531c065cbf
sanjana17r <38381244+sanjana17r@users.noreply.github.com>
2018-05-21 15:00:05 +0530

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,20 @@
+var recipename="creamy squash linguine", time="45 min", recipeby="Jimmy Cutter",
+isvegetarian="true",calories="223.7";
+//variables are assigned for recipe name,time taken,recipe made by, is it vegetarian,and the amount of calories
+console.log('recipe details')
+console.log('------------------')
+console.log('Recipe Name: '+ recipename)
+console.log('Preparation Time: '+ time)
+console.log('Recipe By: '+ recipeby)
+console.log('Is Vegetarian Dish: '+ isvegetarian)
+console.log('Calories: '+ calories)
+//All the variables are logged into console above,
+console.log('Recipe details after update')
+console.log('------------------')
+var time="60 min",recipeby="jimmy cutter george";// time and recipeby variables are updated.
+console.log('Recipe Name: '+ recipename)
+console.log('Preparation Time: '+ time)
+console.log('Recipe By: '+ recipeby)
+console.log('Is Vegetarian Dish: '+ isvegetarian)
+console.log('Calories: '+ calories)
+//the variables are updated and logged into console 
